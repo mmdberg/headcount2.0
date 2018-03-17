6f061f9c3c6b361fc9772c7fec157448911c2127
Michael Gatewood <michael@mngatewood.com>
2018-03-17 16:19:17 -0600

    Adds icons and decreases card size

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import './styles/card.css';
 import PropTypes from 'prop-types';
+import check from './styles/check.png'
+import none from './styles/none.gif'
 
 class Card extends Component {  
 
   yearValues = (stats) => {
     return Object.keys(stats).map(year => {
       let roundedPercent = parseInt((stats[year] * 100));
-      if (roundedPercent < 50) {
-        return <p key={year}>{year}: {roundedPercent}%</p>
-      } else {
-        return <p key={year} className="higher-achievement">{year}: {roundedPercent}%</p>
-      }       
+      let higherAchievement = roundedPercent >= 50;
+      return (
+        <li key={year} className="cardStats">
+          <img className="check" src={higherAchievement ? check : none} />
+          <p className={higherAchievement ? "year higher-achievement" : "year"}>{year}: </p>
+          <p className={higherAchievement ? "year higher-achievement" : "percent"}>{roundedPercent}%</p>
+        </li>
+      );
     });
   }
 
@@ -22,8 +27,12 @@ class Card extends Component {
   render() {
     return (
       <div className={this.props.className} onClick={this.handleClick}>
-        <h2>{this.props.location}</h2>
+        <header>
+          <h2 className="district-title">{this.props.location}</h2>   
+        </header>
+        <ul>
         {this.yearValues(this.props.stats)}
+        </ul>
       </div>
     );
   }
